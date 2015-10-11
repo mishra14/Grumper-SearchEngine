63e70f97d686441fac4f12c6e76538a2237a6b02
mishra14 <mishra14@gmail.com>
2015-10-11 19:51:29 -0400

    fetching files from url

--- src/edu/upenn/cis455/servlet/XPathServlet.java
+++ src/edu/upenn/cis455/servlet/XPathServlet.java
@@ -1,13 +1,20 @@
 package edu.upenn.cis455.servlet;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class XPathServlet extends HttpServlet {
 	
+	private static final String BASE_PATH = "/usr/share/jetty/webapps/";
 	/* TODO: Implement user interface for XPath engine here */
 	
 	/* You may want to override one or both of the following methods */
@@ -15,14 +22,16 @@ public class XPathServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		/* TODO: Implement user interface for XPath engine here */
+		URL sourceUrl = new URL(request.getParameter("url"));
+		String sourceFileName = sourceUrl.getFile();
+		File sourceFile = new File(BASE_PATH+sourceFileName);
+		sourceFile.mkdirs();
+		Path destinationPath = new File(BASE_PATH+sourceFileName).toPath();
+		Files.copy(sourceUrl.openStream(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
+		byte [] fileBytes = Files.readAllBytes(destinationPath);
+		deleteFileAndParents(destinationPath);
 		PrintWriter out = response.getWriter();		
-		out.print("<html><body>"
-					+ request.toString()
-					+ "<br>"
-					+ request.getParameter("xPath")
-					+ "<br>"
-					+ request.getParameter("url")
-					+ "</body></html>");
+		out.print(new String(fileBytes));
 		
 		response.flushBuffer();
 		
@@ -41,12 +50,40 @@ public class XPathServlet extends HttpServlet {
 					+ "<input type=\"text\" name=\"url\">"
 					+ "<br><br>"
 					+ "<input type=\"submit\" value=\"submit\">"
-					+ "</form>"
+					+ "</form><br><br>"
+					+ "Ankit Mishra<br>"
+					+ "mankit<br>"
 					+ "</body></html>");
 		
 		response.flushBuffer();
 	}
-
+	
+	public void deleteFileAndParents(Path filePath) throws IOException
+	{
+		if(filePath==null || filePath.endsWith(BASE_PATH))
+		{
+			return;
+		}
+		else
+		{
+			if(Files.isRegularFile(filePath))
+			{
+				Files.deleteIfExists(filePath);
+			}
+			else if(Files.isDirectory(filePath))
+			{
+				try
+				{
+					Files.deleteIfExists(filePath);
+				}
+				catch(DirectoryNotEmptyException e)
+				{
+					return;
+				}
+			}
+			deleteFileAndParents(filePath.getParent());
+		}
+	}
 }
 
 
