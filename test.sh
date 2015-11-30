bbfa75eda07885a9509704dcfd812422fdd84f0c
CIS 455/555 Student <anwesha@seas.upenn.edu>
2015-11-30 13:56:25 -0500

    changed test script for multinode testing

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 ant clean
 ant crawler
-cp master.war worker.war /usr/share/jetty/webapps
+cp master.war /home/cis455/jetty-master/webapps 
+cp worker.war /home/cis455/jetty-worker1/webapps
+cp worker.war /home/cis455/jetty-worker2/webapps
