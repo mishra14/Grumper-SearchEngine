34fe60dd7733295ccf7feabb98fb9dfd3e18588d
mishra14 <mishra14@gmail.com>
2015-12-05 02:32:01 -0500

    iterative within 1 step and merging of urllists

--- src/edu/upenn/cis455/project/pagerank/Map.java
+++ src/edu/upenn/cis455/project/pagerank/Map.java
@@ -2,6 +2,7 @@ package edu.upenn.cis455.project.pagerank;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.NullWritable;
@@ -11,6 +12,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.upenn.cis455.project.bean.UrlList;
@@ -26,26 +28,32 @@ public class Map extends Mapper<NullWritable, BytesWritable, Text, Text>
 	public void map(NullWritable key, BytesWritable value, Context context)
 			throws IOException, InterruptedException
 	{
-		UrlList urlList = getUrlList(value);
-		if (urlList != null)
+		for (UrlList urlList : getUrlLists(value))
 		{
-			HashSet<String> forwardLinks = (HashSet<String>) urlList.getUrls();
-			String url = urlList.getParentUrl();
-			Item item = dynamo.getItem("hostName", url);
-			Float urlRank = new Float(1);
-			if (item != null)
+			if (urlList != null)
 			{
-				urlRank = item.getFloat("rank");
-			}
-			Float edgeWeight = urlRank / forwardLinks.size();
-			for (String forwardLink : forwardLinks)
-			{
-				Float rankVar = edgeWeight;
-				String rankString = rankVar.toString();
-				context.write(new Text(forwardLink), new Text(rankString));
+				HashSet<String> forwardLinks = (HashSet<String>) urlList
+						.getUrls();
+				String url = urlList.getParentUrl();
+				Item item = dynamo.getItem("hostName", url);
+				Float urlRank = new Float(1);
+				if (item != null)
+				{
+					urlRank = item.getFloat("rank");
+				}
+				Float edgeWeight = urlRank / forwardLinks.size();
+				for (String forwardLink : forwardLinks)
+				{
+					if (forwardLink != null && !forwardLink.isEmpty())
+					{
+						Float rankVar = edgeWeight;
+						String rankString = rankVar.toString();
+						context.write(new Text(forwardLink), new Text(
+								rankString));
+					}
+				}
 			}
 		}
-
 	}
 
 	private UrlList getUrlList(BytesWritable value)
@@ -58,6 +66,26 @@ public class Map extends Mapper<NullWritable, BytesWritable, Text, Text>
 		{
 			urlList = mapper.readValue(new String(value.getBytes()),
 					UrlList.class);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return urlList;
+	}
+
+	private List<UrlList> getUrlLists(BytesWritable value)
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+		List<UrlList> urlList = null;
+		try
+		{
+			urlList = mapper.readValue(new String(value.getBytes()),
+					new TypeReference<List<UrlList>>()
+					{
+					});
 		}
 		catch (IOException e)
 		{
--- src/edu/upenn/cis455/project/pagerank/PageRankTool.java
+++ src/edu/upenn/cis455/project/pagerank/PageRankTool.java
@@ -10,44 +10,61 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
- 
-public class PageRankTool extends Configured implements Tool {
- 
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new PageRankTool(), args);
-        System.exit(res);
-    }
- 
-    @Override
-    public int run(String[] args) throws Exception {
- 
-        // When implementing tool
-        //Configuration conf = this.getConf();
- 
-        // Create job
-        //Job job = new Job(conf, "Tool Job");
-        Job job = Job.getInstance(new Configuration(), PageRankTool.class.getCanonicalName());
-        job.setJarByClass(PageRankTool.class);
- 
-        // Setup MapReduce job
-        // Do not specify the number of Reducer
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
- 
-        // Specify key / value
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
- 
-        // Input
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        job.setInputFormatClass(WholeFileInputFormat.class);
- 
-        // Output
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setOutputFormatClass(TextOutputFormat.class);
- 
-        // Execute job and return status
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
-}
 
+public class PageRankTool extends Configured implements Tool
+{
+
+	private static final int INTERATION_COUNT = 10;
+
+	public static void main(String[] args) throws Exception
+	{
+		int res = 1;
+		for (int i = 0; i < INTERATION_COUNT; i++)
+		{
+			String input = args[0];
+			String output = args[1];
+			String[] jobArgs = { input, output + i };
+			res = ToolRunner.run(new Configuration(), new PageRankTool(),
+					jobArgs);
+			if (res == 0)
+			{
+				break;
+			}
+		}
+		System.exit(res);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception
+	{
+
+		// When implementing tool
+		// Configuration conf = this.getConf();
+
+		// Create job
+		// Job job = new Job(conf, "Tool Job");
+		Job job = Job.getInstance(new Configuration(),
+				PageRankTool.class.getCanonicalName());
+		job.setJarByClass(PageRankTool.class);
+
+		// Setup MapReduce job
+		// Do not specify the number of Reducer
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		// Specify key / value
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		// Input
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		job.setInputFormatClass(WholeFileInputFormat.class);
+
+		// Output
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		// Execute job and return status
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+}
