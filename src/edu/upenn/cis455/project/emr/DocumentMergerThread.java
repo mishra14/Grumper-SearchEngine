74f383cd577f51072b630467a0389490149a95ec
Ankit Mishra <mishra14@gmail.com>
2015-12-07 21:48:28 -0500

    indexer controller

---
+++ src/edu/upenn/cis455/project/emr/DocumentMergerThread.java
@@ -0,0 +1,159 @@
+package edu.upenn.cis455.project.emr;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import edu.upenn.cis455.project.bean.DocumentRecord;
+import edu.upenn.cis455.project.crawler.Hash;
+
+public class DocumentMergerThread extends Thread
+{
+
+	private static final int DOCUMENTS_TO_MERGE = 150;
+	private int id;
+	private String projectDocumentBucket;
+	private String mergedDocumentBucket;
+	private List<String> objectNames;
+	private AmazonS3Client s3Client;
+
+	public DocumentMergerThread(String projectDocumentBucket,
+			String mergedDocumentBucket, List<String> objectNames, int id)
+	{
+		super();
+		this.projectDocumentBucket = projectDocumentBucket;
+		this.mergedDocumentBucket = mergedDocumentBucket;
+		this.objectNames = objectNames;
+		this.s3Client = new AmazonS3Client();
+		this.id = id;
+	}
+
+	public void run()
+	{
+		System.out.println("Merger Thread " + id + " : started");
+		try
+		{
+			mergeCrawledDocuments();
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			System.out.println("Merger Thread " + id + " : Exception");
+			e.printStackTrace();
+		}
+		catch (JsonProcessingException e)
+		{
+			System.out.println("Merger Thread " + id + " : Exception");
+			e.printStackTrace();
+		}
+		System.out.println("Merger Thread " + id + " : done");
+	}
+
+	public void mergeCrawledDocuments() throws NoSuchAlgorithmException,
+			JsonProcessingException
+	{
+		long size = 0;
+		List<DocumentRecord> mergedDocuments = new ArrayList<DocumentRecord>();
+		for (String object : objectNames)
+		{
+			DocumentRecord doc = getDocument(projectDocumentBucket, object);
+			size += doc.toString().length() * 2;
+			mergedDocuments.add(doc);
+			if (mergedDocuments.size() > DOCUMENTS_TO_MERGE)
+			{
+				System.out.println("Merger Thread " + id + " : Merged "
+						+ mergedDocuments.size() + " documents with size - "
+						+ size);
+				// merge the list into 1 s3 object
+				writeDocuments(mergedDocuments, mergedDocumentBucket);
+				// clear the list
+				mergedDocuments.clear();
+				// clear size
+				size = 0;
+			}
+		}
+		if (mergedDocuments.size() > 0)
+		{
+			// merge the list into 1 s3 object
+			writeDocuments(mergedDocuments, mergedDocumentBucket);
+			// clear the list
+			mergedDocuments.clear();
+		}
+	}
+
+	public DocumentRecord getDocument(String bucketName, String prefix)
+	{
+		DocumentRecord doc = null;
+		try
+		{
+			GetObjectRequest req = new GetObjectRequest(bucketName, prefix);
+			S3Object s3Object = s3Client.getObject(req);
+			InputStream objectData = s3Object.getObjectContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					objectData));
+			StringBuilder s3Content = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null)
+			{
+				s3Content.append(line + "\r\n");
+			}
+			reader.close();
+			objectData.close();
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+			mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+			doc = mapper.readValue(s3Content.toString(), DocumentRecord.class);
+		}
+		catch (AmazonS3Exception ase)
+		{
+			System.out.println("Merger Thread " + id
+					+ " : document does not exist");
+			ase.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Merger Thread " + id
+					+ " : IOException while fetching document from S3");
+			e.printStackTrace();
+		}
+		return doc;
+	}
+
+	public String writeDocuments(List<DocumentRecord> mergedDocuments,
+			String bucketName) throws NoSuchAlgorithmException,
+			JsonProcessingException
+	{
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(mergedDocuments);
+		String s3Key = Hash.hashKey(json);
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(
+				json.getBytes());
+		ObjectMetadata omd = new ObjectMetadata();
+		omd.setContentLength(json.getBytes().length);
+		PutObjectRequest request = new PutObjectRequest(bucketName, s3Key,
+				inputStream, omd);
+		s3Client.putObject(request);
+		return s3Key;
+	}
+
+}
--- src/edu/upenn/cis455/project/emr/IndexerEmrController.java
+++ src/edu/upenn/cis455/project/emr/IndexerEmrController.java
@@ -3,52 +3,109 @@ package edu.upenn.cis455.project.emr;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.fasterxml.jackson.core.JsonProcessingException;
-
-import edu.upenn.cis455.project.bean.DocumentRecord;
 
 public class IndexerEmrController
 {
+	private static AmazonS3Client s3Client = new AmazonS3Client();
+	private static String projectDocumentBucket = "edu.upenn.cis455.project.documents";
+	private static String indexerDocumentBucket = "edu.upenn.cis455.project.indexer.documents.large";
+	private static String emrInputPath = "s3://test-indexer/";
+	private static String emrOutputBucketName = "edu.upenn.cis455.project.indexer";
+	private static String emrOutputPrefix = "output";
+	private static String clusterLogPath = "s3://edu.upenn.cis455.project.indexer/log";
+	private static String emrJarPath = "s3://edu.upenn.cis455.project.indexer/code/indexerUnigram.jar";
+	private static String emrStepName = "indexer";
+	private static String clusterId = "j-Q5KFD4DZEMVI";
+	private static String clusterName = "indexer cluster";
+	private static String ec2AccessKeyName = "test";
+	private static String tableName = "IndexerTest";
+	private static String primaryKeyName = "Word";
+	private static String rangeKeyName = "Range";
+	private static String valueKeyName = "Postings";
 
 	public static void main(String[] args) throws NoSuchAlgorithmException,
 			JsonProcessingException, InterruptedException
 	{
-		String documentBucketName = "edu.upenn.cis455.project.documents";
-		String emrInputPath = "s3://test-indexer/";
-		String emrOutputBucketName = "edu.upenn.cis455.project.indexer";
-		String emrOutputPrefix = "output";
-		String clusterLogPath = "s3://edu.upenn.cis455.project.indexer/log";
-		String emrJarPath = "s3://edu.upenn.cis455.project.indexer/code/indexerUnigram.jar";
-		String emrStepName = "indexer";
-		String clusterId = "j-Q5KFD4DZEMVI";
-		String clusterName = "indexer cluster";
-		String ec2AccessKeyName = "test";
-		String tableName = "IndexerTest";
-		String primaryKeyName = "Word";
-		String rangeKeyName = "Range";
-		String valueKeyName = "Postings";
+
 		EmrController controller = new EmrController(emrInputPath,
 				emrOutputBucketName, emrOutputPrefix, emrJarPath, emrStepName,
-				clusterId, tableName, primaryKeyName, valueKeyName, rangeKeyName);
+				clusterId, tableName, primaryKeyName, valueKeyName,
+				rangeKeyName);
 		/*EmrController controller = new EmrController(emrInputPath,
 				emrOutputBucketName, emrOutputPrefix, clusterLogPath,
 				emrJarPath, emrStepName, clusterName, ec2AccessKeyName,
 				tableName, primaryKeyName, valueKeyName, rangeKeyName);
 		controller.createCluster();*/
 		System.out.println(new Date());
-		/*controller.mergeCrawledDocuments(
-				controller.getObjectNamesForBucket(documentBucketName),
-				"edu.upenn.cis455.project.indexer.documents");*/
-		controller.setIterative(false);
-		//controller.runJob();
-		controller.s3ToDynamoPostings(controller.getObjectNamesForBucket());
-		/*List<String> docs = controller
-				.getObjectNamesForBucket("edu.upenn.cis455.project.documents");
-		System.out.println(docs.size());*/
+		List<String> documentsToMerge = getObjectNamesForBucket(projectDocumentBucket);
+		List<DocumentMergerThread> mergerThreads = new ArrayList<DocumentMergerThread>();
+		int i = 0;
+		final int DOCS_TO_MERGE = 30000;
+		while ((i + DOCS_TO_MERGE) < documentsToMerge.size())
+		{
+			DocumentMergerThread mergerThread = new DocumentMergerThread(
+					projectDocumentBucket, indexerDocumentBucket,
+					documentsToMerge.subList(i, i + DOCS_TO_MERGE), i);
+			mergerThreads.add(mergerThread);
+			mergerThread.start();
+			i = i + DOCS_TO_MERGE;
+		}
+		if (i < documentsToMerge.size())
+		{
+			DocumentMergerThread mergerThread = new DocumentMergerThread(
+					projectDocumentBucket, indexerDocumentBucket,
+					documentsToMerge.subList(i, documentsToMerge.size()), i);
+			mergerThreads.add(mergerThread);
+			mergerThread.start();
+		}
+		for (DocumentMergerThread mergerThread : mergerThreads)
+		{
+			mergerThread.join();
+		}
+		// controller.runJob();
+		// controller.s3ToDynamoPostings(controller.getObjectNamesForBucket());
+		List<String> docs = controller
+				.getObjectNamesForBucket(projectDocumentBucket);
+		System.out.println(docs.size());
 		System.out.println("Indexer Terminated");
 		System.out.println(new Date());
+	}
+
+	public static List<String> getObjectNamesForBucket(String bucketName)
+	{
+		long count = 0;
+		ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+				.withBucketName(bucketName);
+		ObjectListing objects = s3Client.listObjects(listObjectsRequest);
+		List<String> objectNames = new ArrayList<String>(objects
+				.getObjectSummaries().size());
+		Iterator<S3ObjectSummary> objectIter = objects.getObjectSummaries()
+				.iterator();
+		while (objectIter.hasNext())
+		{
+			count++;
+			objectNames.add(objectIter.next().getKey());
+		}
+		while (objects.isTruncated())
+		{
+			objects = s3Client.listNextBatchOfObjects(objects);
+			objectIter = objects.getObjectSummaries().iterator();
+			while (objectIter.hasNext())
+			{
+				count++;
+				objectNames.add(objectIter.next().getKey());
+			}
+		}
+		System.out.println("count - " + count);
+		return objectNames;
 	}
 
 }
